c2088d769fa7bbf7d055fc5c95f479ee124c4146
Renzo <renzotacuritm@gmail.com>
2026-07-01 15:41:40 -0500

    Añadido: Alertas de Sesion Incorrecta,Limpieza deL Modulo Login

--- src/app/pages/sesion/login/login.page.ts
+++ src/app/pages/sesion/login/login.page.ts
@@ -1,85 +1,122 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { IonItem,  IonLabel,  IonInput,  IonButton } from '@ionic/angular/standalone';
-import { AuthService } from '../../../services/auth';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+import { AuthService } from '../../../services/auth';
 import { ThemeService } from '../../../services/theme';
 
 import { getApp } from "firebase/app";
-import { getRemoteConfig, fetchAndActivate, getValue, } from "firebase/remote-config";
+import { getRemoteConfig, fetchAndActivate, getValue } from "firebase/remote-config";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],  
-  imports: [ReactiveFormsModule, CommonModule,IonContent,
-  IonHeader,  IonTitle,  IonToolbar,  IonItem,  IonLabel,
-  IonInput,  IonButton ],
+  styleUrls: ['./login.page.scss'],
+  imports: [ ReactiveFormsModule, CommonModule, IonContent, IonHeader, IonTitle,
+    IonToolbar, IonItem, IonLabel, IonInput, IonButton  ],
   standalone: true
 })
 export class LoginPage implements OnInit {
-
+ 
+  // VARIABLES  
   loginForm!: FormGroup;
 
+  
+  // CONSTRUCTOR  
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService, //  inyectamos servicio
+    private authService: AuthService,
     private router: Router,
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private alertController: AlertController
   ) {}
 
+  
+  // CICLO DE VIDA  
   ngOnInit() {
     this.loadThemeFromFirebase();
-    //this.themeService.loadTheme();
+
     this.loginForm = this.fb.group({
       correo: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
+  
+  // MÉTODOS PRINCIPALES  
   async iniciarSesion() {
     if (this.loginForm.invalid) {
-      return alert('Completa todos los campos correctamente');
+      this.loginForm.markAllAsTouched();
+      await this.mostrarAlerta(
+        'Datos Incorrectos',
+        'Completa todos los campos correctamente.'
+      );
+      return;
     }
 
     const { correo, password } = this.loginForm.value;
 
     try {
-      await this.authService.login(correo, password); // usamos servicio
-      alert('Inicio de sesión exitoso');
+      await this.authService.login(correo, password);
+
+      await this.mostrarAlerta(
+        'Éxito',
+        'Inicio de sesión exitoso.'
+      );
+
       this.router.navigate(['/cata-ver']);
+
     } catch (error) {
-      alert('Correo o contraseña incorrectos');
+      await this.mostrarAlerta(
+        'Error',
+        'Correo o contraseña incorrectos.'
+      );
     }
   }
+
   irARegistro() {
-  this.router.navigate(['/register']);
-}
+    this.router.navigate(['/register']);
+  }
+
   irACatalogo() {
     this.router.navigate(['/cata-ver']);
   }
+
   setTheme(theme: string) {
-  this.themeService.setTheme(theme);
-}
-
-async loadThemeFromFirebase() {
-  const remoteConfig = getRemoteConfig(getApp());
-
-  remoteConfig.settings = {
-  minimumFetchIntervalMillis: 0,
-  fetchTimeoutMillis: 60000
-  };
-
-  await fetchAndActivate(remoteConfig);
-
-  const theme = getValue(remoteConfig, "app_theme").asString();
-
-  console.log("Tema desde Firebase:", theme);
-
-  if (theme) {
-    this.setTheme(theme);
+    this.themeService.setTheme(theme);
   }
-}
+
+  
+  // UTILIDADES  
+  async mostrarAlerta(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
+
+  async loadThemeFromFirebase() {
+    const remoteConfig = getRemoteConfig(getApp());
+
+    remoteConfig.settings = {
+      minimumFetchIntervalMillis: 0,
+      fetchTimeoutMillis: 60000
+    };
+
+    await fetchAndActivate(remoteConfig);
+
+    const theme = getValue(remoteConfig, "app_theme").asString();
+
+    console.log("Tema desde Firebase:", theme);
+
+    if (theme) {
+      this.setTheme(theme);
+    }
+  }
 }
